479175f274b5b5217b0c9aab0b95c8132c714c7b
Thiago Fonseca <thiagofonsecasantanasouza@gmail.com>
2022-05-09 15:33:56 -0300

    Laçoes Repeticoes

---
+++ ExFor1.java
@@ -0,0 +1,16 @@
+package Repeticao;
+
+public class ExFor1
+{
+
+	public static void main(String[] args)
+	{
+		for(int x = 1000; x <= 1999; x++)
+		{
+			if(x%11==5)
+				System.out.println(x);
+		}
+
+	}
+
+}
